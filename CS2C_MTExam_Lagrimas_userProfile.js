752f9fbafca1887520151976a7e21c94486683fe
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-10-31 10:24:54 +0800

    CS2C_MTExam_Lagrimas_userProfile.js

---
+++ CS2C_MTExam_Lagrimas_userProfile.js
@@ -0,0 +1,9 @@
+let fullName = prompt("Karryle B. Lagrimas:");
+let age = prompt("19:");
+let favoriteNumber = prompt("20:");
+let favoriteColor = prompt("Pink:");
+
+console.log("Karryle B. Lagrimas:", fullName);
+console.log("19:", age);
+console.log("20:", favoriteNumber);
+console.log("Pink:", favoriteColor);
